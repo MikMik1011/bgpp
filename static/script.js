0edf979abbef54e970cd3571a8d7c37348be85e1
Misa Stefanovic <mikmikyt1011@gmail.com>
2023-07-18 14:14:52 +0200

    add station pin on map, fix single-digit secons, remove lowercasing

--- static/script.js
+++ static/script.js
@@ -2,6 +2,24 @@ let interval;
 let map, layerGroup;
 let lastStationId = 0;
 
+const stationIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -14],
+  shadowSize: [41, 41],
+});
+
+const formatSeconds = (seconds) => {
+  let minutes = Math.floor(seconds / 60);
+  let secondsLeft = seconds % 60;
+  if (secondsLeft < 10) secondsLeft = `0${secondsLeft}`;
+  return `${minutes}:${secondsLeft}`;
+};
+
 const updateDisplay = (city, id, recenter) => {
   let url = `/api/stations/${city}/${id}`;
   $("#updateInProgress").show();
@@ -13,39 +31,35 @@ const updateDisplay = (city, id, recenter) => {
       let date = new Date();
       response.vehicles.reverse();
 
-      $("#stationName")
-        .html(`${response.name} (${response.id})`);
-      $("#lastUpdated").html(`Poslednji put ažurirano: ${date.toLocaleTimeString()}`);
+      let name = `${response.name} (${response.id})`;
+      $("#stationName").html(name);
+      $("#lastUpdated").html(
+        `Poslednji put ažurirano: ${date.toLocaleTimeString()}`
+      );
       $("#updateInProgress").hide();
-        
 
       layerGroup.clearLayers();
-      console.log(response.coords)
-      if (recenter)
-        map.setView(
-          response.coords,
-          13,
-          { animation: true }
-        );
+      console.log(response.coords);
+      if (recenter) map.setView(response.coords, 13, { animation: true });
+
+      let marker = new L.marker(response.coords, { icon: stationIcon });
+      marker.addTo(layerGroup);
 
       const tableData = response.vehicles
         .map((value) => {
-
           let marker = new L.marker(value.coords);
-          marker.bindTooltip(value.lineNumber.toLowerCase(), {
+          marker.bindTooltip(value.lineNumber, {
             permanent: true,
             direction: "center",
             className: "my-labels",
           });
           marker.addTo(layerGroup);
           return `<tr>
-                        <td>${value.lineNumber.toLowerCase()}</td>
-                        <td>${Math.floor(value.secondsLeft / 60)}:${
-            value.secondsLeft % 60
-          }</td>
+                        <td>${value.lineNumber}</td>
+                        <td>${formatSeconds(value.secondsLeft)}</td>
                         <td>${value.stationsBetween}</td>
-                        <td>${value.stationName.toLowerCase()}</td>
-                        <td>${value.garageNo.toLowerCase()}</td>
+                        <td>${value.stationName}</td>
+                        <td>${value.garageNo}</td>
                     </tr>`;
         })
         .join("");
@@ -65,7 +79,6 @@ $(document).ready(function () {
   });
   layerGroup = L.layerGroup().addTo(map);
 
-  
   L.control.layers(mapLayers).addTo(map);
   mapLayers.Transport.addTo(map);
 
