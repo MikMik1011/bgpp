4963d43ac85a2ef5624af3ec68bcf29cc454c5d5
Misa Stefanovic <mikmikyt1011@gmail.com>
2023-07-30 23:02:18 +0200

    code revamp, prepare for adding new features

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 let currInterval;
 let map, layerGroup;
-let lastStationId = 0;
+let currStationUID;
+let allStations = {};
 
 const stationIcon = new L.Icon({
   iconUrl:
@@ -12,6 +13,28 @@ const stationIcon = new L.Icon({
   popupAnchor: [1, -14],
   shadowSize: [41, 41],
 });
+
+const fetchCityStations = (city) => {
+  let url = `/api/stations/${city}/all`;
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: (response) => {
+      allStations[city] = response;
+      console.log(allStations);
+    },
+    error: (error) => {
+      console.error("Error sending request:", error);
+    },
+  });
+};
+
+const onCityChange = () => {
+  let city = encodeURIComponent($("#city").val());
+  changeBg(city);
+  moveMapToCityCentre(city);
+  if (!allStations[city]) fetchCityStations(city);
+};
 
 const cityCentres = {
   bg: [44.81254796404323, 20.46145496621977],
@@ -36,27 +59,66 @@ const spawnInterval = () => {
   if (!id) return;
 
   let city = encodeURIComponent($("#city").val());
-  updateDisplay(city, id, true);
+  fetchArrivals(city, id, true);
 
   currInterval = clearInterval(currInterval);
   currInterval = setInterval(() => {
-    updateDisplay(city, id, false);
+    fetchArrivals(city, id, false);
   }, 10 * 1000);
 };
 
 const handleTabOut = () => {
   if (!$("#dataSaver").is(":checked")) return;
-  console.log("tab out")
+  console.log("tab out");
   clearInterval(currInterval);
 };
 
 const handleTabIn = () => {
   if (!$("#dataSaver").is(":checked")) return;
-  console.log("tab in")
+  console.log("tab in");
   spawnInterval();
 };
 
-const updateDisplay = (city, id, recenter) => {
+const updateArrivals = (response, recenter) => {
+  let date = new Date();
+  response.vehicles.reverse();
+
+  let name = `${response.name} (${response.id})`;
+  $("#stationName").html(name);
+  $("#lastUpdated").html(
+    `Poslednji put ažurirano: ${date.toLocaleTimeString()}`
+  );
+  $("#updateInProgress").hide();
+
+  layerGroup.clearLayers();
+  console.log(response.coords);
+  if (recenter) map.setView(response.coords, 13, { animation: true });
+
+  let marker = new L.marker(response.coords, { icon: stationIcon });
+  marker.addTo(layerGroup);
+
+  const tableData = response.vehicles
+    .map((value) => {
+      let marker = new L.marker(value.coords);
+      marker.bindTooltip(value.lineNumber, {
+        permanent: true,
+        direction: "center",
+        className: "my-labels",
+      });
+      marker.addTo(layerGroup);
+      return `<tr>
+                    <td>${value.lineNumber}</td>
+                    <td>${formatSeconds(value.secondsLeft)}</td>
+                    <td>${value.stationsBetween}</td>
+                    <td>${value.stationName}</td>
+                    <td>${value.garageNo}</td>
+                </tr>`;
+    })
+    .join("");
+  $("#tableBody").html(tableData);
+};
+
+const fetchArrivals = (city, id, recenter) => {
   let url = `/api/stations/${city}/search?id=${id}`;
   $("#updateInProgress").show();
   $("#error").hide();
@@ -64,45 +126,10 @@ const updateDisplay = (city, id, recenter) => {
   $.ajax({
     url: url,
     type: "GET",
-    success: function (response) {
-      let date = new Date();
-      response.vehicles.reverse();
-
-      let name = `${response.name} (${response.id})`;
-      $("#stationName").html(name);
-      $("#lastUpdated").html(
-        `Poslednji put ažurirano: ${date.toLocaleTimeString()}`
-      );
-      $("#updateInProgress").hide();
-
-      layerGroup.clearLayers();
-      console.log(response.coords);
-      if (recenter) map.setView(response.coords, 13, { animation: true });
-
-      let marker = new L.marker(response.coords, { icon: stationIcon });
-      marker.addTo(layerGroup);
-
-      const tableData = response.vehicles
-        .map((value) => {
-          let marker = new L.marker(value.coords);
-          marker.bindTooltip(value.lineNumber, {
-            permanent: true,
-            direction: "center",
-            className: "my-labels",
-          });
-          marker.addTo(layerGroup);
-          return `<tr>
-                        <td>${value.lineNumber}</td>
-                        <td>${formatSeconds(value.secondsLeft)}</td>
-                        <td>${value.stationsBetween}</td>
-                        <td>${value.stationName}</td>
-                        <td>${value.garageNo}</td>
-                    </tr>`;
-        })
-        .join("");
-      $("#tableBody").html(tableData);
+    success: (response) => {
+      updateArrivals(response, recenter);
     },
-    error: function (error) {
+    error: (error) => {
       console.error("Error sending request:", error);
       $("#updateInProgress").hide();
       $("#error").show();
@@ -110,10 +137,7 @@ const updateDisplay = (city, id, recenter) => {
   });
 };
 
-$(document).ready(function () {
-  $(window).on("blur", handleTabOut);
-  $(window).on("focus", handleTabIn);
-
+const initMap = () => {
   map = L.map("map", {
     center: [44.81254796404323, 20.46145496621977],
     zoom: 13,
@@ -122,23 +146,31 @@ $(document).ready(function () {
 
   L.control.layers(mapLayers).addTo(map);
   mapLayers.Transport.addTo(map);
+};
+
+$(document).ready(() => {
+  $(window).on("blur", handleTabOut);
+  $(window).on("focus", handleTabIn);
+
+  initMap();
 
   $.ajax({
     url: "/api/cities",
     type: "GET",
-    success: function (response) {
+    success: (response) => {
       let cities = Object.entries(response).map(([key, value]) => {
         return `<option value="${key}">${value}</option>`;
       });
       $("#city").html(cities);
+      onCityChange();
     },
-    error: function (error) {
+    error: (error) => {
       console.error("Error sending request:", error);
       // Handle error here
     },
   });
 
-  $("#myForm").submit(function (event) {
+  $("#myForm").submit((event) => {
     event.preventDefault(); // Prevent form from being submitted
     spawnInterval();
   });
