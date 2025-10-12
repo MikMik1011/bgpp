bb724d177b1eb98809e0d458768408aa357192af
Miša Stefanović <mikmikyt1011@gmail.com>
2025-10-12 15:45:06 +0200

    feat: bounding boxes calculation for stations

--- src/routes/api/v2/cities/[city]/stations/+server.ts
+++ src/routes/api/v2/cities/[city]/stations/+server.ts
@@ -1,8 +1,9 @@
 import type { BusLogicAPI } from '$lib/buslogic/api/BusLogicAPI';
 import { error, json, type RequestEvent } from '@sveltejs/kit';
 import { cacheRunner, getInstance } from '../../../busLogicManager';
+import { addBoundingBoxesToStations } from './utils';
 
-export const GET = async ({ params, fetch }: RequestEvent) => {
+export const GET = async ({ params, url }: RequestEvent) => {
 	if (!params.city) {
 		return error(400, 'City is required');
 	}
@@ -15,6 +16,19 @@ export const GET = async ({ params, fetch }: RequestEvent) => {
 		await cacheRunner.addFunction(city, api.getAllStations.bind(api));
 	}
 	const stations = await cacheRunner.get(city);
+	if (!stations) {
+		return error(500, 'Failed to retrieve stations');
+	}
+
+	if(url.searchParams.has('radius')) {
+		const radius = parseInt(url.searchParams.get('radius') || '0');
+		if(isNaN(radius) || radius <= 0) {
+			return error(400, 'Radius must be a positive number');
+		}
+		
+		const stationsWithBoundingBox = addBoundingBoxesToStations(Object.values(stations), radius);
+		return json(stationsWithBoundingBox);
+	}
 
 
 	return json(Object.values(stations));
--- src/routes/api/v2/cities/[city]/stations/[stationId]/+server.ts
+++ src/routes/api/v2/cities/[city]/stations/[stationId]/+server.ts
@@ -1,8 +1,9 @@
 import type { BusLogicAPI } from '$lib/buslogic/api/BusLogicAPI';
 import { error, json, type RequestEvent } from '@sveltejs/kit';
 import { cacheRunner, getInstance } from '../../../../busLogicManager';
+import { addBoundingBoxesToStations } from '../utils';
 
-export const GET = async ({ params, fetch }: RequestEvent) => {
+export const GET = async ({ params, url }: RequestEvent) => {
 	if (!params.city) {
 		return error(400, 'City is required');
 	}
@@ -18,9 +19,23 @@ export const GET = async ({ params, fetch }: RequestEvent) => {
 		await cacheRunner.addFunction(city, api.getAllStations.bind(api));
 	}
 	const stations = await cacheRunner.get(city);
+	if (!stations) {
+		return error(500, 'Failed to retrieve stations');
+	}
+
 	const station = stations[params.stationId.toUpperCase() ?? '0'];
 	if (!station) {
 		return error(404, `Station ID ${params.stationId} not found`);
+	}
+
+	if(url.searchParams.has('radius')) {
+		const radius = parseInt(url.searchParams.get('radius') || '0');
+		if(isNaN(radius) || radius <= 0) {
+			return error(400, 'Radius must be a positive number');
+		}
+		
+		const stationWithBoundingBox = addBoundingBoxesToStations([station], radius)[0];
+		return json(stationWithBoundingBox);
 	}
 
 	return json(station);
