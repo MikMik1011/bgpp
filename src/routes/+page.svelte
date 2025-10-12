98a45604d31b10ec4cf33ac927d5c2e6865e2ea0
Miša Stefanović <mikmikyt1011@gmail.com>
2025-10-12 02:49:36 +0200

    refactor: api routes now go into cities/{city}

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,24 @@
-<h1>work in progress</h1>
-<h2>api v2 only for now!</h2>
-<ul>
-    <li><code>/api/v2/[city]/stations</code> – get all stations in the city</li>
-    <li><code>/api/v2/[city]/stations/hash</code> – get hash value for all stations response</li>
-    <li><code>/api/v2/[city]/stations/[stationId]/arrivals</code> – get arrivals for that station</li>
-    <li><code>/api/v2/cities</code> – get list of all supported cities</li>
-</ul>
+<main>
+    <h1>Work in progress, API v2 only for now!</h1>
+    <br>
+    <h2>API v2 Endpoints</h2>
+    <br>
+
+    <h3>Cities</h3>
+    <ul>
+        <li><code>/api/v2/cities</code> – Get a list of all supported cities</li>
+    </ul>
+    <br>
+
+    <h3>Stations</h3>
+    <ul>
+        <li><code>/api/v2/cities/[city]/stations</code> – Get all stations in a city</li>
+        <li><code>/api/v2/cities/[city]/stations/hash</code> – Get hash value for all stations in a city</li>
+    </ul>
+    <br>
+
+    <h3>Arrivals</h3>
+    <ul>
+        <li><code>/api/v2/cities/[city]/stations/[stationId]/arrivals</code> – Get arrivals for a specific station</li>
+    </ul>
+</main>
