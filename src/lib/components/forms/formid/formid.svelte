7de9913323cee10c3008245c1671862ea514093a
Miša Stefanović <mikmikyt1011@gmail.com>
2026-09-24 11:30:47 +0200

    nit: change placeholder example

--- src/lib/components/forms/formid/formid.svelte
+++ src/lib/components/forms/formid/formid.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div>
-	<Input bind:value={idValue} placeholder="Broj stanice (npr. P81524)" class="w-full" />
+	<Input bind:value={idValue} placeholder="Broj stanice (npr. 0402A)" class="w-full" />
 
 	<Dialog.Trigger asChild let:builder>
 		<Button builders={[builder]} class="w-full mt-2" disabled={!idValue.trim()}>
